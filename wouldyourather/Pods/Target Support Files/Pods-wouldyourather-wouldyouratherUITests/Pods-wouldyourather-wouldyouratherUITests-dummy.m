86499cef1bcd460f264a9d252d6eab6b68ce810d
Matthew Nguyen <matthewnguyen@ucsb.edu>
2022-10-22 11:51:20 -0700

    pod files initialization

---
+++ wouldyourather/Pods/Target Support Files/Pods-wouldyourather-wouldyouratherUITests/Pods-wouldyourather-wouldyouratherUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wouldyourather_wouldyouratherUITests : NSObject
+@end
+@implementation PodsDummy_Pods_wouldyourather_wouldyouratherUITests
+@end
---
+++ wouldyourather/Pods/Target Support Files/Pods-wouldyouratherTests/Pods-wouldyouratherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wouldyouratherTests : NSObject
+@end
+@implementation PodsDummy_Pods_wouldyouratherTests
+@end
