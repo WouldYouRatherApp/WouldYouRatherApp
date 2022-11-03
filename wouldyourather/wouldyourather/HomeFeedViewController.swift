5fb8ff640628e3016fcac57023f0c86ccc29e034
Heather Nguyen <htn@umail.ucsb.edu>
2022-11-03 14:13:40 -0700

    in progress

--- wouldyourather/wouldyourather/HomeFeedViewController.swift
+++ wouldyourather/wouldyourather/HomeFeedViewController.swift
@@ -6,17 +6,34 @@
 //
 
 import UIKit
+import Parse
 
-class HomeFeedViewController: UIViewController {
+class HomeFeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var questionTableView: UITableView!
     
+    var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     override func viewDidLoad() {
+        questionTableView.dataSource = self
+        questionTableView.delegate = self
+
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // change later
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "row: \(indexPath.row)"
+        
+        return  cell
+    }
 
     /*
     // MARK: - Navigation
