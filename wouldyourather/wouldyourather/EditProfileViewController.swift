77e1b9de7a46a6949036484532305a1f07f8ab34
francisco <franciscolira408@gmail.com>
2022-11-04 23:15:35 -0700

    added UI Components to Profile and Edit Profile pages

---
+++ wouldyourather/wouldyourather/EditProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EditProfileViewController.swift
+//  wouldyourather
+//
+//  Created by Francisco Lira on 11/4/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var vButtonAction: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
